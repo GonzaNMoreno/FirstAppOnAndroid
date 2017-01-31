a29619f9c16c49b5e307b4159ee3cf5cf00e1641
Gonzalo Nicolas Moreno <g.moreno@globant.com>
2017-01-31 14:43:49 -0300

    Second activity, con un user and pass trucheli

--- app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.gmoreno.myfirstapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button secondActivity = (Button) findViewById(R.id.btnSecondActivity);
+        secondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
--- app/src/main/java/com/example/gmoreno/myfirstapplication/SecondActivity.java
+++ app/src/main/java/com/example/gmoreno/myfirstapplication/SecondActivity.java
@@ -1,9 +1,32 @@
 package com.example.gmoreno.myfirstapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by g.moreno on 1/31/2017.
  */
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        final EditText userName = (EditText) findViewById(R.id.txtUser);
+        final EditText password = (EditText) findViewById(R.id.txtPassword);
+
+        Button loggeo = (Button) findViewById(R.id.btnIngresar);
+        loggeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this, "El usuario es: " + userName.getText().toString() +
+                        " y tu contraseña es: " + password.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
