9dfa69c6da799fda8fc57587262d8f3fdf944c6b
Gonzalo Nicolas Moreno <g.moreno@globant.com>
2017-01-31 16:03:29 -0300

    Shushi

--- app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,21 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.firstApp:
+                Log.e("Primer item", "Error en primer item");
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
