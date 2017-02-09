8a6b9a9e88e05e51fd3e9bef580d4ac5af61be0f
Gonzalo Nicolas Moreno <g.moreno@globant.com>
2017-02-09 17:22:09 -0300

    Adding testing module

--- app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/gmoreno/myfirstapplication/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.gmoreno.myfirstapplication;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetingView = (TextView) findViewById(R.id.greeting);
 
         Button secondActivity = (Button) findViewById(R.id.btnSecondActivity);
         secondActivity.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
+        inflater.inflate(R.menu.activity_main, menu);
+
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+
         return true;
     }
 
@@ -42,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.item_option:
                 Log.e("Primer item", "Error en primer item");
                 break;
+            case R.id.action_bye:
+                //To Do: say bye
+                greetingView.setText(R.string.bye);
             default:
                 break;
         }
